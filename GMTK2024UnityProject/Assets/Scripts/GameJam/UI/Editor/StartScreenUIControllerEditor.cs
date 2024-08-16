eb06b9e63cb2c12fd659d8477a875bf91e4f06a9
redseiko <redseiko@gmail.com>
2024-08-16 13:53:39 -0700

    Add GameJam.Editor asmdef and a basic StartScreenUIController editor window.

---
+++ GMTK2024UnityProject/Assets/Scripts/GameJam/UI/Editor/StartScreenUIControllerEditor.cs
@@ -0,0 +1,28 @@
+using UnityEditor;
+
+using UnityEngine;
+
+namespace GameJam.Editor {
+  [CustomEditor(typeof(StartScreenUIController))]
+  public sealed class StartScreenUIControllerEditor : UnityEditor.Editor {
+    StartScreenUIController _controller;
+
+    private void OnEnable() {
+      _controller = (StartScreenUIController) target;
+    }
+
+    public override void OnInspectorGUI() {
+      base.OnInspectorGUI();
+      EditorGUILayout.Separator();
+
+      EditorGUILayout.LabelField(nameof(StartScreenUIController), EditorStyles.boldLabel);
+      EditorGUILayout.Separator();
+
+      using (new EditorGUI.DisabledScope(!Application.isPlaying)) {
+        if (GUILayout.Button("AnimateIntro")) {
+          _controller.AnimateIntro();
+        }
+      }
+    }
+  }
+}
--- GMTK2024UnityProject/Assets/Scripts/GameJam/UI/StartScreenUIController.cs
+++ GMTK2024UnityProject/Assets/Scripts/GameJam/UI/StartScreenUIController.cs
@@ -3,6 +3,8 @@ using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
+using YoloBox;
+
 namespace GameJam {
   public sealed class StartScreenUIController : MonoBehaviour {
     [field: SerializeField]
@@ -23,13 +25,25 @@ namespace GameJam {
     }
 
     public void AnimateIntro() {
+      DOTween.Complete(gameObject, withCallbacks: true);
+
       DOTween.Sequence()
           .SetTarget(gameObject)
-          .Insert(0f, CreateTranslateFade(GameLogo, 0.25f, new(0f, -200f, 0f), 0f, 3f))
-          .Insert(1f, CreateTranslateFade(StartImage, 0.25f, new(0f, 50f, 0f), 0f, 3f))
-          .Insert(1f, CreateTranslateFade(SettingsImage, 0.75f, new(-50f, 0f, 0f), 0f, 3f))
-          .Insert(1f, CreateTranslateFade(CreditsImage, 1.25f, new(-50f, 0f, 0f), 0f, 3f))
+          .Insert(0f, CreateTranslateFade(GameLogo, 0.25f, new(0f, -200f, 0f), 0f, 2.5f))
+          .Insert(0.5f, CreateTranslateFade(StartImage, 0.25f, new(0f, 50f, 0f), 0f, 3f))
+          .Insert(0.5f, CreateTranslateFade(SettingsImage, 0.75f, new(-50f, 0f, 0f), 0f, 3f))
+          .Insert(0.5f, CreateTranslateFade(CreditsImage, 1.25f, new(-50f, 0f, 0f), 0f, 3f))
           .SetEase(Ease.InOutQuad);
+    }
+
+    private Sequence _gameLogoOnClickTween;
+
+    private void CreateTweens() {
+      _gameLogoOnClickTween =
+          DOTween.Sequence()
+              .SetAutoKill(false)
+              .Insert(0f, GameLogo.transform.DOPunchScale(Vector3.one * 0.05f, 0.5f, 10, 1f))
+              .Pause();
     }
 
     private Sequence CreateTranslateFade(
@@ -45,19 +59,8 @@ namespace GameJam {
                   .From(fadeFrom));
     }
 
-    private Sequence _gameLogoOnClickTween;
-
-    private void CreateTweens() {
-      _gameLogoOnClickTween =
-          DOTween.Sequence()
-              .SetAutoKill(false)
-              .Insert(0f, GameLogo.transform.DOPunchScale(Vector3.one * 0.05f, 0.5f, 10, 1f))
-              .Pause();
-    }
-
     public void OnGameLogoClick() {
-      _gameLogoOnClickTween.Complete();
-      _gameLogoOnClickTween.Restart();
+      _gameLogoOnClickTween.PlayAgain();
     }
   }
 }
