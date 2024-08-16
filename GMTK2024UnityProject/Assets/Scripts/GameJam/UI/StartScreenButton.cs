601ec343e1c4444d8927c0a5c58e7a57a07a99b9
redseiko <redseiko@gmail.com>
2024-08-16 14:08:05 -0700

    More tweaks to StartScreenUI.

---
+++ GMTK2024UnityProject/Assets/Scripts/GameJam/UI/StartScreenButton.cs
@@ -0,0 +1,45 @@
+using DG.Tweening;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+using YoloBox;
+
+namespace GameJam {
+  public sealed class StartScreenButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    [field: SerializeField]
+    public Image Image { get; private set; }
+
+    [field: Header("OnHover")]
+    [field: SerializeField]
+    public float TweenToDuration { get; private set; }
+
+    [field: SerializeField]
+    public Vector3 TweenToScale { get; private set; }
+
+    [field: SerializeField]
+    public Color TweenToColor { get; private set; }
+
+    private Sequence _onHoverTween;
+
+    private void Start() {
+      _onHoverTween =
+          DOTween.Sequence()
+              .Insert(0f, transform.DOScale(TweenToScale, TweenToDuration))
+              .Insert(0f, Image.DOColor(TweenToColor, TweenToDuration))
+              .SetEase(Ease.InOutQuad)
+              .SetAutoKill(false)
+              .SetUpdate(true)
+              .Pause();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+      _onHoverTween.PlayAgain();
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+      _onHoverTween.SmoothRewind();
+    }
+  }
+}
