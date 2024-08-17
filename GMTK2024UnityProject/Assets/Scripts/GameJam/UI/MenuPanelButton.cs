c22fb7cffb01b50aa6cd02444cdb86cbfd87dcb0
redseiko <redseiko@gmail.com>
2024-08-16 19:28:47 -0700

    Create MenuPanelButton.

---
+++ GMTK2024UnityProject/Assets/Scripts/GameJam/UI/MenuPanelButton.cs
@@ -0,0 +1,52 @@
+using DG.Tweening;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+using YoloBox;
+using TMPro;
+
+namespace GameJam {
+  public sealed class MenuPanelButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    [field: Header("UI")]
+    [field: SerializeField]
+    public Image Image { get; private set; }
+
+    [field: SerializeField]
+    public TextMeshProUGUI Label { get; private set; }
+
+    [field: Header("OnHover")]
+    [field: SerializeField]
+    public float TweenToDuration { get; private set; }
+
+    [field: SerializeField]
+    public Vector3 TweenToScale { get; private set; }
+
+    [field: SerializeField]
+    public Color TweenToColor { get; private set; }
+
+    private Sequence _onHoverTween;
+
+    private void Start() {
+      _onHoverTween =
+          DOTween.Sequence()
+              .SetLink(gameObject)
+              .Insert(0f, Label.transform.DOScale(TweenToScale, TweenToDuration))
+              .Insert(0f, Label.transform.DOLocalMoveY(5f, TweenToDuration).SetRelative())
+              .Insert(0f, Image.DOColor(TweenToColor, TweenToDuration))
+              .SetEase(Ease.InOutQuad)
+              .SetAutoKill(false)
+              .SetUpdate(true)
+              .Pause();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+      _onHoverTween.PlayAgain();
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+      _onHoverTween.SmoothRewind();
+    }
+  }
+}
