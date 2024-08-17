6f61c5fe3372bf4606ee95d528e107a9c8599186
redseiko <redseiko@gmail.com>
2024-08-16 20:03:05 -0700

    Create InteractManager and Interactable and assign to Venture scene.

---
+++ GMTK2024UnityProject/Assets/Scripts/GameJam/Game/Interactable.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace GameJam {
+  public class Interactable : MonoBehaviour {
+    [field: SerializeField, Header("Interact")]
+    public float InteractRange { get; set; } = 2f;
+
+    [field: SerializeField]
+    public bool CanInteract { get; set; } = true;
+
+    [field: SerializeField]
+    public string InteractText { get; set; } = string.Empty;
+
+    [field: SerializeField, Header("Highlight")]
+    public MeshRenderer HighlightRenderer { get; set; }
+
+    [Header("Events")]
+    [Space(10f)]
+    public UnityEvent<GameObject> OnInteract;
+
+    public void Interact() {
+      OnInteract?.Invoke(default);
+    }
+
+    public void Interact(GameObject interactAgent) {
+      OnInteract?.Invoke(interactAgent);
+    }
+  }
+}
---
+++ GMTK2024UnityProject/Assets/Scripts/GameJam/Game/Managers/InteractManager.cs
@@ -0,0 +1,75 @@
+using System;
+
+using UnityEngine;
+
+using YoloBox;
+
+namespace GameJam {
+  public sealed class InteractManager : SingletonManager<InteractManager> {
+    [field: Header("Agent")]
+    [field: SerializeField]
+    public GameObject InteractAgent { get; private set; }
+
+    [field: SerializeField, Min(0f)]
+    public float InteractRange { get; set; } = 4f;
+
+    [field: Header("Interact")]
+    [field: SerializeField]
+    public bool CanInteract { get; set; } = true;
+
+    [field: SerializeField]
+    public Interactable ClosestInteractable { get; private set; }
+
+    private void FixedUpdate() {
+      Interactable interactable =
+          CanInteract && InteractAgent ? GetClosestInteractable(InteractAgent.transform, InteractRange) : default;
+
+      if (interactable != ClosestInteractable) {
+        // TODO(redseiko@): add Facepunch.Highlight package to make use of this.
+        // HighlightManager.Instance.RemoveInteractable(ClosestInteractable);
+        // HighlightManager.Instance.AddInteractable(interactable);
+
+        ClosestInteractable = interactable;
+      }
+    }
+
+    private readonly RaycastHit[] _raycastHits = new RaycastHit[20];
+    private readonly float[] _hitDistanceCache = new float[20];
+
+    private Interactable GetClosestInteractable(Transform origin, float range) {
+      int count =
+          Physics.SphereCastNonAlloc(
+              origin.position,
+              0.25f,
+              origin.forward,
+              _raycastHits,
+              range,
+              -1,
+              QueryTriggerInteraction.Ignore);
+
+      if (count <= 0) {
+        return default;
+      }
+
+      for (int i = 0; i < count; i++) {
+        _hitDistanceCache[i] = _raycastHits[i].distance;
+      }
+
+      Array.Sort(_hitDistanceCache, _raycastHits, 0, count);
+
+      for (int i = 0; i < count; i++) {
+        RaycastHit raycastHit = _raycastHits[i];
+        Interactable interactable = raycastHit.collider.GetComponentInParent<Interactable>();
+
+        if (interactable
+            && interactable.enabled
+            && interactable.CanInteract
+            && raycastHit.distance <= interactable.InteractRange) {
+          return interactable;
+        }
+      }
+
+      return default;
+    }
+  }
+}
