fa3162a92c8fcd6b95a34e39036cd1dce052b223
redseiko <redseiko@gmail.com>
2024-08-18 15:45:05 -0700

    Add GameManager.SetUserCount(); wire to LevelController.

--- GMTK2024UnityProject/Assets/Scripts/GameJam/Game/Managers/GameManager.cs
+++ GMTK2024UnityProject/Assets/Scripts/GameJam/Game/Managers/GameManager.cs
@@ -1,9 +1,15 @@
+using UnityEngine;
+
 using YoloBox;
 
 namespace GameJam {
   public sealed class GameManager : SingletonManager<GameManager> {
     private void Start() {
       UIManager.Instance.CalloutPanels(calloutDuration: 5f);
+    }
+
+    public void SetUserCount(int userCount) {
+      UIManager.Instance.SetCurrentUserCount(userCount);
     }
   }
 }
