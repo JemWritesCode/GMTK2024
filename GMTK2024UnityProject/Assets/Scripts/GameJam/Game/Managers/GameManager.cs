336a5003cf725b6da49802bfc0f3b7063adff02f
redseiko <redseiko@gmail.com>
2024-08-19 17:27:23 -0700

    Modify GameManager to pause game whenever MenuPanel or DialogPanel is visible.

--- GMTK2024UnityProject/Assets/Scripts/GameJam/Game/Managers/GameManager.cs
+++ GMTK2024UnityProject/Assets/Scripts/GameJam/Game/Managers/GameManager.cs
@@ -11,6 +11,10 @@ namespace GameJam {
     [field: SerializeField]
     public DSDialogueSO StartingDialogNode { get; private set; }
 
+    [field: Header("Pause")]
+    [field: SerializeField]
+    public bool IsGamePaused { get; private set; }
+
     private void Start() {
       StartCoroutine(StartGame());
     }
@@ -31,6 +35,24 @@ namespace GameJam {
 
     public void SetUserCount(int userCount) {
       UIManager.Instance.SetCurrentUserCount(userCount);
+    }
+
+    private void Update() {
+      SetIsGamePaused(ShouldPauseGame());
+    }
+
+    public void SetIsGamePaused(bool toggleOn) {
+      if (toggleOn && !IsGamePaused) {
+        IsGamePaused = true;
+        Time.timeScale = 0f;
+      } else if (!toggleOn && IsGamePaused) {
+        IsGamePaused = false;
+        Time.timeScale = 1f;
+      }
+    }
+
+    public bool ShouldPauseGame() {
+      return UIManager.Instance.MenuPanel.IsPanelVisible || UIManager.Instance.DialogPanel.IsPanelVisible;
     }
   }
 }
