a3ef11db5c220d77bc241290c32db0de23804d22
redseiko <redseiko@users.noreply.github.com>
2024-09-01 16:17:09 -0700

    Modify CableEndPoint to colorize based on connected/unconnected status.

--- GMTK2024UnityProject/Assets/Scripts/GameJam/Game/CableEndPoint.cs
+++ GMTK2024UnityProject/Assets/Scripts/GameJam/Game/CableEndPoint.cs
@@ -1,105 +1,139 @@
 using UnityEngine;
+
 using static GameJam.CableType;
 
-namespace GameJam
-{
-    public class CableEndPoint : MonoBehaviour
-    {
-        public CableStartPoint Connection;
-        public CableBoxType Type = CableBoxType.None;
-        public AudioClip cableEndPointSound;
-        public AudioClip wrongCableTypeSound;
-        AudioSource cableEndPointAudioSource;
+namespace GameJam {
+  public class CableEndPoint : MonoBehaviour {
+    public CableStartPoint Connection;
+    public CableBoxType Type = CableBoxType.None;
+    public AudioClip cableEndPointSound;
+    public AudioClip wrongCableTypeSound;
+    AudioSource cableEndPointAudioSource;
 
-        public GameObject CableAttachPoint;
-        public GameObject FlameEffects;
-        public GameObject DataEffects;
+    public GameObject CableAttachPoint;
+    public GameObject FlameEffects;
+    public GameObject DataEffects;
 
-        private void Start()
-        {
-            cableEndPointAudioSource = GetComponent<AudioSource>();
-        }
+    [field: Header("Color")]
+    [field: SerializeField]
+    public Renderer EndPointRenderer { get; private set; }
 
-        public Vector3 GetCableAttachPoint()
-        {
-            if (CableAttachPoint == null)
-            {
-                return this.transform.position;
-            }
+    [field: SerializeField]
+    public Color UnconnectedColor { get; private set; } = Color.white;
 
-            return CableAttachPoint.transform.position;
-        }
+    [field: SerializeField]
+    public Color DataConnectedColor { get; private set; } = Color.cyan;
 
-        public bool IsConnected()
-        {
-            return Connection != null;
-        }
+    [field: SerializeField]
+    public Color PowerConnectedColor { get; private set; } = Color.yellow;
 
-        public void BreakConnection(bool playEffects)
-        {
-            if (IsConnected())
-            {
-                Connection.BreakConnection();
-                if (playEffects)
-                {
-                    ParticleSystem[] effects = null;
-                    if (Type == CableBoxType.Data)
-                    {
-                        effects = DataEffects.GetComponentsInChildren<ParticleSystem>();
-                    }
-                    else if (Type == CableBoxType.Power)
-                    {
-                        effects = FlameEffects.GetComponentsInChildren<ParticleSystem>();
-                    }
-
-                    if (effects != null)
-                    {
-                        foreach (var effect in effects)
-                        {
-                            effect.Play();
-                        }
-                    }
-                }
-            }
-        }
-
-        public void CancelConnection()
-        {
-            if (IsConnected())
-            {
-                Connection.StartConnection();
-                Connection = null;
-            }
-        }
-
-        public void CompleteConnection(CableStartPoint cable)
-        {
-            Connection = cable;
-            cable.CompleteConnection(this);
-            if (cableEndPointSound && cableEndPointAudioSource)
-            {
-                cableEndPointAudioSource.PlayOneShot(cableEndPointSound);
-            }
-        }
-
-        public void CableInteract(GameObject interactAgent)
-        {
-            if (HandManager.Instance.HoldingCable())
-            {
-
-                if (!IsConnected() && HandManager.Instance.CurrentCable.Type == Type)
-                {
-                    CompleteConnection(HandManager.Instance.CurrentCable);
-                }
-                else if (!(HandManager.Instance.CurrentCable.Type == Type))
-                {
-                    cableEndPointAudioSource.PlayOneShot(wrongCableTypeSound, .5f);
-                }
-            }
-            else if (IsConnected())
-            {
-                CancelConnection();
-            }
-        }
+    private static readonly int _colorShaderId = Shader.PropertyToID("_Color");
+    private static MaterialPropertyBlock _propertyBlock;
+    private static MaterialPropertyBlock GetSharedPropertyBlock() {
+      return _propertyBlock ??= new();
     }
+
+    private void Start() {
+      cableEndPointAudioSource = GetComponent<AudioSource>();
+      SetEndPointColor(UnconnectedColor);
+    }
+
+    private void OnValidate() {
+      SetEndPointColor(UnconnectedColor);
+    }
+
+    public void SetEndPointColor(Color color) {
+      MaterialPropertyBlock propertyBlock = GetSharedPropertyBlock();
+      EndPointRenderer.GetPropertyBlock(propertyBlock);
+      propertyBlock.SetColor(_colorShaderId, color);
+      EndPointRenderer.SetPropertyBlock(propertyBlock);
+    }
+
+    private void RefreshEndPointColor() {
+      if (IsConnected()) {
+        Color color =
+            Type switch {
+              CableBoxType.Data => DataConnectedColor,
+              CableBoxType.Power => PowerConnectedColor,
+              _ => Color.white
+            };
+
+        SetEndPointColor(color);
+      } else {
+        SetEndPointColor(UnconnectedColor);
+      }
+    }
+
+    public Vector3 GetCableAttachPoint() {
+      if (CableAttachPoint == null) {
+        return this.transform.position;
+      }
+
+      return CableAttachPoint.transform.position;
+    }
+
+    public bool IsConnected() {
+      return Connection != null;
+    }
+
+    public void BreakConnection(bool playEffects) {
+      if (IsConnected()) {
+        Connection.BreakConnection();
+
+        if (playEffects) {
+          PlayBreakConnectionEffects();
+        }
+      }
+
+      RefreshEndPointColor();
+    }
+
+    private void PlayBreakConnectionEffects() {
+      ParticleSystem[] effects = null;
+
+      if (Type == CableBoxType.Data) {
+        effects = DataEffects.GetComponentsInChildren<ParticleSystem>();
+      } else if (Type == CableBoxType.Power) {
+        effects = FlameEffects.GetComponentsInChildren<ParticleSystem>();
+      }
+
+      if (effects != null) {
+        foreach (var effect in effects) {
+          effect.Play();
+        }
+      }
+    }
+
+    public void CancelConnection() {
+      if (IsConnected()) {
+        Connection.StartConnection();
+        Connection = null;
+      }
+
+      RefreshEndPointColor();
+    }
+
+    public void CompleteConnection(CableStartPoint cable) {
+      Connection = cable;
+      cable.CompleteConnection(this);
+      if (cableEndPointSound && cableEndPointAudioSource) {
+        cableEndPointAudioSource.PlayOneShot(cableEndPointSound);
+      }
+
+      RefreshEndPointColor();
+    }
+
+    public void CableInteract(GameObject interactAgent) {
+      if (HandManager.Instance.HoldingCable()) {
+
+        if (!IsConnected() && HandManager.Instance.CurrentCable.Type == Type) {
+          CompleteConnection(HandManager.Instance.CurrentCable);
+        } else if (!(HandManager.Instance.CurrentCable.Type == Type)) {
+          cableEndPointAudioSource.PlayOneShot(wrongCableTypeSound, .5f);
+        }
+      } else if (IsConnected()) {
+        CancelConnection();
+      }
+    }
+  }
 }
